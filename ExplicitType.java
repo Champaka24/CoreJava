c2110d1463437750e89f5e2598a00976e610fe0b
Champaka24 <krchampaka23@gmail.com>
2024-09-20 20:20:34 +0530

    Explicit Type Casting

---
+++ ExplicitType.java
@@ -0,0 +1,12 @@
+public class ExplicitType {
+    public static void main(String[] args) {
+
+        double d = 10.54;
+        int i = (int) d;
+
+        System.out.println(d);
+        System.out.println(i);
+
+    }
+
+}
