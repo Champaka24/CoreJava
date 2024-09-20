9817be380c4b54a20640443a27ff5c0ac0c2ab3d
Champaka0208 <krchampaka23@gmail.com>
2024-09-20 06:37:53 +0530

    Implicit Type

---
+++ ImplicitType.java
@@ -0,0 +1,14 @@
+public class ImplicitType{
+    public static void main(String[] args) {
+        
+        int a=10;
+        float b=a;
+        double c=a;
+        long d=a;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
