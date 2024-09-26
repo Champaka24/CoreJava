14a7d77c84710467a08fa843b2758e6951d04390
Champaka24 <krchampaka23@gmail.com>
2024-09-26 06:39:41 +0530

    Sum of Natural Numbers

--- SumOfNat.java
+++ SumOfNat.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-class SumOfNaturalN{
-	public int calculateSum(int n) {
+class SumOfNatNum{
+	public int CalculateSum(int n) {
 		int sum=0;
 		for(int i=0;i<=n;i++) {
 			sum+=i;
@@ -9,15 +9,15 @@ class SumOfNaturalN{
 		return sum;
 	}
 	public void printSum(int n) {
-		int sum=calculateSum(n);
-		System.out.println("The sum of first "+n+" natural numbers is: "+sum);
+		int Sum=CalculateSum(n);
+		System.out.println("The sum of first "+n+" natural numbers is: "+Sum);
 	}
 }
 
 public class SumOfNat{
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
-		SumOfNaturalN sn=new SumOfNaturalN();
+		SumOfNatNum sn=new SumOfNatNum();
 
 		System.out.print("Enter the number: ");
 		int num=scanner.nextInt();
